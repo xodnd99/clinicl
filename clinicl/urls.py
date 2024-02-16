c7379f6e0e46e2baad6757f03dea7b9b79da39df
ghostqaw <esbolyegizekov@gmail.com>
2024-02-16 19:43:55 +0600

    requirements

--- clinicl/urls.py
+++ clinicl/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for clinicl project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='html/index.html'), name='home'),  # URL для отображения index.html
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
--- clinicl/views.py
+++ clinicl/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'html/index.html')
+
+
+from django.contrib.auth.views import LoginView
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
